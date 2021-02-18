1652edf100dea95ddc9c6ac8469ab09f92a5dfd3
WhoAmMe <yanghepingcc@qq.com>
2021-02-18 06:45:22 +0000

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,4 +33,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Topic::class);
     }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
